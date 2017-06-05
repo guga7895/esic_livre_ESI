c4434bb229cd411e3edb9a9f6fd03342aa3eed47
Ariel Palmeira <arielpalmeiracontato@gmail.com>
2017-06-05 00:09:51 -0300

    Comentario e README mais descritivos
    Signed-off-by: Ariel Palmeira <arielpalmeiracontato@gmail.com>

--- settings/local_settings.openshift_example.py
+++ settings/local_settings.openshift_example.py
@@ -6,10 +6,26 @@ SQLALCHEMY_DATABASE_URI = (
         user=os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'],
         password=os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD'],
         host=os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'],
-        port=os.environ['OPENSHIFT_POSTGRESQL_DB_PORT']))
+        port=os.environ['OPENSHIFT_POSTGRESQL_DB_PORT']
+    )
+)
 
+# Credenciais usadas para interagir com o eSIC do municipio de São Paulo
 ESIC_EMAIL = "{email}"
-ESIC_PASSWORD = "{esic_password}"
-FIREFOX_PATH = "../firefox/firefox"
-DOWNLOADS_PATH = "/path/where/to/store/ff/downloads"
+ESIC_PASSWORD = "{senha}"
+
+# Caminho para o executavel do firefox usaod pelo selenium
+# MUITO IMPORTANTE NOTAR QUE O CAMINHO DEVE APONTAR DIRETAMENTE PARA
+# O EXECUTAVEL DO PROGRAMA
+FIREFOX_PATH = "/path/to/firefox/executable"
+
+# Caminho para uma pasta onde serão guardados temporariamente os arquivos .wav
+# baixados para quebrar o catcha do eSIC
+DOWNLOADS_PATH = "/path/to/downloads"
+
+# Prefixo de onde serão guardados os arquivos em anexo mandados do eSIC do
+# municipio de Sao Paulo como resposta de volta para o esiclivre
 ATTACHMENT_URL_PREFIX = '{prefix}'
+
+# Pasta para armazenar os logs do sistema
+LOG_FOLDER = '/path/to/LOG_FOLDER'
